a707a1439f578c0c305c3aa5200c477d21e5acb0
iKnock <ergana.henok@gmail.com>
2021-01-12 18:56:26 +0100

    portfolio data update personal info

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
 
-    if(this.props.data){
+    if(this.props.data){ 
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
@@ -17,7 +17,6 @@ class Header extends Component {
       <header id="home">
 
       <nav id="nav-wrap">
-
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
@@ -26,16 +25,13 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
-
       </nav>
 
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I'm a {city} based <span>{occupation}</span>. {description}</h3>
             <hr />
             <ul className="social">
                {networks}
