9c3953706cbfddf1bfc1abcd2852127906ea5b13
iKnock <ergana.henok@gmail.com>
2021-01-12 20:38:35 +0100

    remove yarn lock

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,6 +17,7 @@ class Header extends Component {
       <header id="home">
 
       <nav id="nav-wrap">
+         
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
